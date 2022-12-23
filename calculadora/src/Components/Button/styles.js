42efd90312297521e3a3c7743d631a3f627f6ca6
Eugênio Francisco da Silva <eugeniofr17@gmail.com>
2022-12-23 16:36:31 -0300

    Second commit

---
+++ calculadora/src/Components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 18px;
+    border: 1px solid #CDCDCD;
+    background-color: #00AAFD;
+    color: #FFFFFF;
+    font-size: 28px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover{
+        opacity: 0.6;
+    }
+`
